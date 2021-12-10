66df9c477ca96a5e9c30ff59354711962ebe9cea
YASSINE EL-MAHI <yassine.elmahi@keiken-digital.com>
2021-12-10 16:56:12 +0100

    map reduce

---
+++ orchestrator/index.js
@@ -0,0 +1,31 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an HTTP starter function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable activity function (default name is "Hello")
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your 
+ *    function app in Kudu
+ */
+
+const df = require("durable-functions");
+
+module.exports = df.orchestrator(function* (context) {
+    const outputs = [];
+
+    const body = context.bindingData.input.text.split(" ");
+
+    const threePartIndex = body.length / 3;
+
+    const thirdPart = body.splice(-threePartIndex);
+    const secondPart = body.splice(-threePartIndex);
+    const firstPart = body;
+
+    outputs.push(yield context.df.callActivity("worker1", firstPart));
+    outputs.push(yield context.df.callActivity("worker2", secondPart));
+    outputs.push(yield context.df.callActivity("worker3", thirdPart));
+    return outputs.flat().reduce((result, n)=>{
+        return result + n ;
+    });
+});
---
+++ worker1/index.js
@@ -0,0 +1,16 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an orchestrator function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable orchestration function
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your
+ *   function app in Kudu
+ */
+
+module.exports = async function (context) {
+    return context.bindings.name.map(function (w){
+        return w.length
+    });
+};
